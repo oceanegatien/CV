3043289f09b156ef7a70f15a47560ff49ad623d6
oceanegatien <oceane.gatien.cef@hotmail.fr>
2017-09-14 16:20:20 +0200

    finitions css

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
 
 $('.parallax-window').parallax({imageSrc: 'img/gatien2.jpg'});
 
-console.log("coucou")
-
 
 window.onresize = resize;
 
-function resize()
-{
+function resize() {
   if($(window).width()<631){
     $("header #headermobile").removeClass('hide');
   }
