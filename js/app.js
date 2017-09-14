18f5728a7e44874e6a061ba2dd5c119db6ae2f2c
oceanegatien <oceane.gatien.cef@hotmail.fr>
2017-09-14 15:25:11 +0200

    correction js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,14 @@ $('.parallax-window').parallax({imageSrc: 'img/gatien2.jpg'});
 console.log("coucou")
 
 
+window.onresize = resize;
 
+function resize()
+{
   if($(window).width()<631){
     $("header #headermobile").removeClass('hide');
   }
+
+}
+
+resize();
